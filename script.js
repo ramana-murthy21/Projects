f6803da67c0f42edd3b5328cb7ea5334d4a7ffdf
Ramana  Murthy <82810077+ramana-murthy21@users.noreply.github.com>
2025-12-29 00:04:01 +0530

    Add click event listener

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll('.dock-item').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.dock-item.active').classList.remove('active');
+    item.classList.add('active');
+  });
+});
